b11f51173a37111b66e9fbd013f119b8f9c0916c
egorkqq <egorkqq@gmail.com>
2025-05-27 11:44:31 +0300

    chore: set version to 0.0.1 and display app version and commit hash in account route

--- apps/main/src/routes/_withMenu/account.tsx
+++ apps/main/src/routes/_withMenu/account.tsx
@@ -11,6 +11,12 @@ function RouteComponent() {
 			Hello "/account"!
 			<Link to="/onboarding">Onboarding</Link>
 			<TonConnectButton />
+			<code className="mt-auto flex flex-col items-center justify-center text-caption-1 text-text-secondary">
+				<div>Point </div>
+				<div>
+					v{__APP_VERSION__} at {__COMMIT_HASH__}
+				</div>
+			</code>
 		</div>
 	)
 }
