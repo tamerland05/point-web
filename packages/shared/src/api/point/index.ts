3c76ca6e9e531a2f7ff844501150a90dfe04af82
egorkqq <egorkqq@gmail.com>
2025-06-05 18:57:32 +0300

    fix: update API endpoint constant for point API integration

--- packages/shared/src/api/point/index.ts
+++ packages/shared/src/api/point/index.ts
@@ -1,10 +1,10 @@
 import axios from "axios"
 
-// export const DEDUST_API_FQDN = "http://84.201.150.47:8000/api"
-export const DEDUST_API_FQDN = "/point-api"
+export const POINT_API_FQDN = "http://84.201.150.47:8000/api"
+// export const POINT_API_FQDN = "/point-api"
 
 const pointAxiosInstance = axios.create({
-	baseURL: `${DEDUST_API_FQDN}/v1`,
+	baseURL: `${POINT_API_FQDN}/v1`,
 	headers: {
 		"Content-Type": "application/json",
 	},
