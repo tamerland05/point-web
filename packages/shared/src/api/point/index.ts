cd3208d0fc6280c2c5651ba67bb6676d4cee28ea
egorkqq <egorkqq@gmail.com>
2025-06-06 17:37:15 +0300

    fix: correct POINT_API_FQDN to use the full API URL for point API integration

--- packages/shared/src/api/point/index.ts
+++ packages/shared/src/api/point/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-// export const POINT_API_FQDN = "https://point-dev-back.meyson.tech/api"
-export const POINT_API_FQDN = "/point-api"
+export const POINT_API_FQDN = "https://point-dev-back.meyson.tech/api"
+// export const POINT_API_FQDN = "/point-api"
 
 const pointAxiosInstance = axios.create({
   baseURL: `${POINT_API_FQDN}/v1`,
