e5d1da055a2177b73f5c29f9f0ba1897e26c3c9d
egorkqq <egorkqq@gmail.com>
2025-06-06 17:34:31 +0300

    refactor: update Places API request structure and improve type definitions for better clarity

--- packages/shared/src/api/point/index.ts
+++ packages/shared/src/api/point/index.ts
@@ -1,13 +1,13 @@
 import axios from "axios"
 
-export const POINT_API_FQDN = "http://84.201.150.47:8000/api"
-// export const POINT_API_FQDN = "/point-api"
+// export const POINT_API_FQDN = "https://point-dev-back.meyson.tech/api"
+export const POINT_API_FQDN = "/point-api"
 
 const pointAxiosInstance = axios.create({
-	baseURL: `${POINT_API_FQDN}/v1`,
-	headers: {
-		"Content-Type": "application/json",
-	},
+  baseURL: `${POINT_API_FQDN}/v1`,
+  headers: {
+    "Content-Type": "application/json",
+  },
 })
 
 export default pointAxiosInstance
--- packages/shared/src/api/point/places.ts
+++ packages/shared/src/api/point/places.ts
@@ -1,97 +1,71 @@
 import { queryOptions } from "@tanstack/react-query"
 
 import pointAxiosInstance from "@/api/point"
+import type { Coordinates } from "@/types"
+import type { AxiosResponse } from "axios"
 
-export interface PlacesReq {
-	upper: {
-		latitude: number
-		longitude: number
-		address: string
-	}
-	lower: {
-		latitude: number
-		longitude: number
-		address: string
-	}
+interface PlacesReq {
+  diagonal: {
+    upper: Coordinates
+    lower: Coordinates
+  }
+  location: Coordinates
 }
 
-export interface Place {
-	id: string
-	name: string
-	photo: string
-	establishmentId: string
-	position: {
-		latitude: number
-		longitude: number
-		address: string
-	}
-	rating: number
+interface PlaceDTO {
+  id: string
+  name: string
+  photo: string
+  establishmentId: string
+  position: Coordinates
+  rating: number
 }
 
-export type PlacesDTO = Place[]
+export const placesQueryOptions = (upper: Coordinates, lower: Coordinates, location: Coordinates) =>
+  queryOptions({
+    queryKey: ["places", upper, lower, location],
+    queryFn: async () => {
+      const response = await pointAxiosInstance.post<PlaceDTO[], AxiosResponse<PlaceDTO[]>, PlacesReq>(
+        "/point/map/places",
+        { diagonal: { upper, lower }, location }
+      )
 
-interface Coordinates {
-	longitude: number
-	latitude: number
-}
+      return response.data
+    },
+  })
 
-export const placesQueryOptions = (upperCoordinates: Coordinates, lowerCoordinates: Coordinates) =>
-	queryOptions({
-		queryKey: ["places", upperCoordinates, lowerCoordinates],
-		queryFn: async () => {
-			const response = await pointAxiosInstance.post<PlacesDTO>("/point/map/places", {
-				upper: {
-					longitude: upperCoordinates.longitude,
-					latitude: upperCoordinates.latitude,
-					address: "",
-				},
-				lower: {
-					longitude: lowerCoordinates.longitude,
-					latitude: lowerCoordinates.latitude,
-					address: "",
-				},
-			})
-
-			return response.data
-		},
-	})
-
-export interface PlaceDTO {
-	id: string
-	name: string
-	photo: string
-	establishmentId: string
-	position: {
-		latitude: number
-		longitude: number
-		address: string
-	}
-	rating: number
-	description: string
-	icon: string
-	gallery: string[]
-	menu: Array<{
-		title: string
-		description: string
-		photo: string
-		cost: {
-			value: number
-			currency: string
-		}
-	}>
+export interface DetailedPlaceDTO {
+  id: string
+  name: string
+  photo: string
+  establishmentId: string
+  position: Coordinates
+  rating: number
+  description: string
+  icon: string
+  gallery: string[]
+  menu: Array<{
+    title: string
+    description: string
+    photo: string
+    cost: {
+      value: number
+      currency: string
+    }
+  }>
 }
 
 export const placeQueryOptions = (placeId?: string) =>
-	queryOptions({
-		queryKey: ["place", placeId],
-		enabled: !!placeId,
-		queryFn: async () => {
-			if (!placeId) {
-				return null
-			}
+  queryOptions({
+    queryKey: ["place", placeId],
+    enabled: !!placeId,
+    queryFn: async () => {
+      if (!placeId) {
+        return null
+      }
 
-			const response = await pointAxiosInstance.post<PlaceDTO>(`/point/map/place/${placeId}`)
+      const response = await pointAxiosInstance.post<DetailedPlaceDTO>(`/point/map/place/${placeId}`)
 
-			return response.data
-		},
-	})
+      return response.data
+    },
+  })
