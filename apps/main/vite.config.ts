2c11f9d14648f34b926ef42b952babea78995ad9
egorkqq <egorkqq@gmail.com>
2025-06-13 18:46:31 +0300

    chore: test vite envs

--- apps/main/vite.config.ts
+++ apps/main/vite.config.ts
@@ -44,9 +44,13 @@ export default defineConfig({
   },
 
   define: {
+    "process.env": "process.env",
     __APP_VERSION__: JSON.stringify(packageConfig.version),
     __COMMIT_HASH__: JSON.stringify(commitHash),
   },
 
   envDir: "../../",
 })
+
+console.log("process.env", process.env)
+console.log("import.meta.env", import.meta.env)
