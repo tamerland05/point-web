0d0969f4a8b31d72b5ef1c5a85d18a225e12cc71
egorkqq <egorkqq@gmail.com>
2025-07-03 11:58:48 +0300

    fix: improve error handling display on ErrorPage component

--- apps/main/src/components/app-internals/ErrorPage.tsx
+++ apps/main/src/components/app-internals/ErrorPage.tsx
@@ -1,6 +1,6 @@
 import { isAxiosError } from "@point/shared/utils/isAxiosError"
 import { cn } from "@point/ui/cn"
-import { ErrorComponent, useRouter } from "@tanstack/react-router"
+import { useRouter } from "@tanstack/react-router"
 
 // мы не смогли найти что вы искали и кнопки обновить (попробоавать еще раз) и назад (вернуться назад)
 export const ErrorPage = ({ error }: { error?: Error }) => {
@@ -25,17 +25,23 @@ export const ErrorPage = ({ error }: { error?: Error }) => {
       <div className=" -translate-x-1/2 -translate-y-1/2 justify-cente absolute top-1/2 left-1/2 flex w-full flex-col items-center">
         <img alt="Telegram sticker" src="/not-found.webp" className="mb-5 block h-36 w-36" />
         <h1 className="mb-2 font-semibold text-title-2">
-          {isAxiosErrorProvided ? error.status : error?.name || "Technical Problems"}
+          {isAxiosErrorProvided
+            ? `${error.response?.statusText} [${error.response?.status}]`
+            : error?.name || "Technical Problems"}
         </h1>
-        <p className={cn("max-w-[300px] text-base text-text-secondary leading-snug")}>
+        <pre
+          className={cn(
+            "max-h-[200px] max-w-[300px] overflow-y-auto whitespace-pre-wrap text-left text-base text-text-secondary leading-snug"
+          )}
+        >
           {isAxiosErrorProvided
             ? // @ts-expect-error it can be here
               error.response?.data?.message ||
+              JSON.stringify(error.response?.data, null, 2) ||
               error.message ||
               "Oops! There were technical problems. We are solving the problem."
             : "Oops! There were technical problems. We are solving the problem."}
-        </p>
-        {error && <ErrorComponent error={error} />}
+        </pre>
       </div>
 
       <div className="mt-auto flex gap-2">
